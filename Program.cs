40001b7012a9cdccc22fdad9c5a60dcbcfc622e0
JohnyWander <j.myslinski.pl@gmail.com>
2022-01-26 10:11:00 +0100

    v2.0

--- Program.cs
+++ Program.cs
@@ -2,17 +2,22 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Reflection;
 
 namespace dotnet_read_emb_res
 {
+
+    
+
+
 
     public static class read_emb_resource
     {
         static public string result_string;
         static public Stream result_stream;
         static public byte[] result_byte_array;
-        
+        static private readonly bool dontmindme=false;
         static public readonly int option_result_string = 1;
         static public readonly int option_result_stream = 2;
         static public readonly int option_result_byte_array = 3;
@@ -94,40 +99,159 @@ namespace dotnet_read_emb_res
 
 
 
-
-
-
-
-
-
-
-
-
-    /*
-    class Program
+    public static class read_emb_resource_async
     {
-        static void Main(string[] args)
+        static public string result_string;
+        static public Stream result_stream;
+        static public byte[] result_byte_array;
+        static private readonly bool dontmindme = false;
+        static public readonly int option_result_string = 1;
+        static public readonly int option_result_stream = 2;
+        static public readonly int option_result_byte_array = 3;
+        static public readonly int option_result_all = 4;
+
+
+        static async private Task<string> get_resource_path(string res_name, Assembly executing_assembly)
         {
-          read_emb_resource.read("grumpy_cat.jpg",static_read_embedded_resource_sync.option_result_string);
-            static_read_embedded_resource_sync.read_embedded_resource_sync("grumpy_cat.jpg", static_read_embedded_resource_sync.option_result_stream);
-
-          //  Console.WriteLine(static_read_embedded_resource_sync.result_string);
-
-
-           // StreamReader s = new StreamReader(static_read_embedded_resource_sync.result_stream);
-           // Console.WriteLine(s.ReadToEnd());
-
-
-            static_read_embedded_resource_sync.read_embedded_resource_sync("grumpy_cat.jpg", static_read_embedded_resource_sync.option_result_byte_array);
-
-            foreach(byte b in static_read_embedded_resource_sync.result_byte_array)
+            
+           var x = await Task<string>.Run(() =>
             {
-                Console.WriteLine(b);
-            }
+                string output = executing_assembly.GetManifestResourceNames().Single(str => str.EndsWith(res_name));
+                return output;
+            });
+
+            return x;
+        }
+
+
+        static private async Task read_to_string(Stream result_stream1)
+        {
+            await Task.Run(() =>
+            {
+                StreamReader reader = new StreamReader(result_stream1);
+                result_string = reader.ReadToEnd();
+                reader.Dispose();
+                reader.Close();
+            });
+
+        }
+
+        static private async Task read_to_stream(Stream result_stream2)
+        {
+            await Task.Run(() =>
+                {
+                    result_stream = result_stream2;
+                });
+        }
+
+        static private async Task read_to_byte_array(Stream read_stream3)
+        {
+
+
+            await Task.Run(() =>
+            {
+
+
+                byte[] buffer = new byte[16 * 1024];
+                MemoryStream stre = new MemoryStream();
+                int read;
+                while ((read = read_stream3.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    stre.Write(buffer, 0, read);
+                }
+
+                result_byte_array = stre.ToArray();
+
+            });
+
+        }
+
+
+
+
+        public static async Task read(string resourcename, int option)
+        {
+            await Task.Run( async() =>
+            {
+
+
+                Assembly executing_assembly = Assembly.GetEntryAssembly();
+                Task<string> res_of_get_resource=get_resource_path(resourcename, executing_assembly);
+               
+                string name_of_resource = res_of_get_resource.Result.ToString();
+                Stream result_stream = executing_assembly.GetManifestResourceStream(name_of_resource);
+
+                if (option == 1)
+                {
+                    read_to_string(result_stream);
+                }
+                else if (option == 2)
+                {
+                    read_to_stream(result_stream);
+                }
+                else if (option == 3)
+                {
+                    read_to_byte_array(result_stream);
+                }
+                else if (option == 4)
+                {
+                    read_to_string(result_stream);
+                    read_to_stream(result_stream);
+                    read_to_byte_array(result_stream);//
+                }
+                else
+                {
+                    throw new Exception("No such option, try to use (1,2,3,4) or option_result_* ints");
+                }
+
+
+
+            //    Thread.Sleep(5000);
+              //  Console.WriteLine("wykonano");
+
+
+            });
+
+
 
 
 
         }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+ // example of using asynchronous version of the class
+ /*
+        class Program
+        {
+            static async Task Main(string[] args)
+            {
+            var task = read_emb_resource_async.read("grumpy_cat.jpg", read_emb_resource.option_result_string);
+
+            Thread.Sleep(4000);
+            Console.WriteLine("1");
+
+            await task;
+
+            Console.WriteLine(read_emb_resource_async.result_string);
+
+
+
+
+
+
+           }
+ 
+        }
+ */    
     }
- */   
-}
